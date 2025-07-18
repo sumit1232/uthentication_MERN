b10146cc7b057f591c0daa3ac812fdf358efb516
Sumit Jadhav <sumitjadav2525@gmail.com>
2025-07-18 20:30:48 +0530

    formHandling

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Login = () => {
+const [loginData,setLoginData]=useState({
+  username:'',
+  password:''
+})
+
+const submitForm=async(e)=>{
+ try {
+  e.preventDefault();
+  const result =await axios.post('http://localhost:3000/login',loginData);
+  console.log(result);
+ } catch (error) {
+  console.log(error);
+ }
+ setLoginData(
+  {username:'',
+  password:''}
+ )
+}
+
+const changeData=(e)=>{
+  const {name,value}=e.target;
+  setLoginData((prevData)=>({
+    ...prevData,
+    [name]:value
+  }))
+  console.log("change data");
+}
+
   return (
-    <div>Login</div>
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Login to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6" onSubmit={submitForm}>
+            <div>
+              <label htmlFor="text" className="block text-sm/6 font-medium text-gray-900">
+                name address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="username"
+                  type="text"
+                  onChange={changeData}
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+             onChange={changeData}
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm/6 text-gray-500">
+            click here to  ?{' '}
+            <a href="/register" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              register
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
 
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Register = () => {
+  const [registerData, setRegisterData] = useState({
+    username: '',
+    password: ''
+  })
+
+  const submitForm = async (e) => {
+    try {
+      e.preventDefault();
+      const result = await axios.post('http://localhost:3000/register', registerData);
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+    setRegisterData({
+      username: '',
+      password: ''
+    })
+  }
+
+  const changeData = (e) => {
+    const {name,value}=e.target;
+    setRegisterData((prevData)=>({
+      ...prevData,
+      [name]:value
+    }))
+    console.log('data change');
+  }
+
+
   return (
-    <div>Register</div>
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6" onSubmit={submitForm}>
+            <div>
+              <label htmlFor="username" className="block text-sm/6 font-medium text-gray-900">
+                username address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  value={registerData.username}
+                  onChange={changeData}
+                  required
+                  autoComplete="username"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={registerData.password}
+                  onChange={changeData}
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm/6 text-gray-500">
+            Already registered ?{' '}
+            <a href="/" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              Login here
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
   )
+
 }
 
 export default Register
